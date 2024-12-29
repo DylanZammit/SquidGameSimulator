ea82723d06d1622c73e353f7b067b869e61c746f
Dylan Zammit <dylanzam@gmail.com>
2024-12-29 11:05:38 +0100

    minor adjustments

--- src/entities/games/game_show.py
+++ src/entities/games/game_show.py
@@ -4,13 +4,22 @@ from entities.player import Player
 
 
 class GameShow(Game):
-    def __init__(self, players: Set[Player], game_list: List[Type[Game]]):
+
+    def __init__(self, players: Set[Player], game_list: List[Type[Game]], max_prize_pool: int = 456_000_000):
         super().__init__(players=players)
         self.players = players
         self.game_list = game_list
         self.active = set(players)
         self.eliminated = set()
         self.games_played = []
+        self.max_prize_pool = max_prize_pool
+        self.prize_cost_per_player = max_prize_pool / self.num_players
+        self.prize_pool = 0
+        self.prize_per_player = 0
+
+    def update_prize_pool(self):
+        self.prize_pool = self.prize_cost_per_player * self.num_eliminated
+        self.prize_per_player = self.prize_pool / self.num_active
 
     def fight(self):
         pass
@@ -23,9 +32,23 @@ class GameShow(Game):
             g = MiniGame(players=self.active)
 
             g.play()
+            self.games_played.append(g)
+
             self.active = g.active
             self.eliminated |= g.eliminated
 
             self.vote()
             self.fight()
-            self.games_played.append(g)
+
+            self.update_prize_pool()
+
+    def __repr__(self):
+        return f'''{type(self).__name__}
+        =============================
+        {self.num_players=}, 
+        {self.num_active=}, 
+        {self.num_eliminated=}, 
+        {self.survival_ratio=:.2f}
+        {self.prize_pool=:,.0f}€
+        {self.prize_per_player=:,.0f}€
+        ============================='''
--- src/simulator.py
+++ src/simulator.py
@@ -1,8 +1,9 @@
 from entities.games import RedLightGreenLight, SugarHoneycombs, TugOfWar, Marbles, GameShow, GlassStones
+from entities.games.squid_game import SquidGame
 from utils import gen_players
 
 
-def main(n_players: int = 456):
+def main(n_players: int = 456, n_sims: int = 1):
 
     players = gen_players(n_players)
     game_list = [
@@ -11,14 +12,15 @@ def main(n_players: int = 456):
         TugOfWar,
         Marbles,
         GlassStones,
-        # SquidGame,
+        SquidGame,
     ]
 
-    game_show = GameShow(players=players, game_list=game_list)
-    game_show.play()
-    print(game_show)
-    for g in game_show.games_played:
-        print(g)
+    for _ in range(n_sims):
+        game_show = GameShow(players=players, game_list=game_list)
+        game_show.play()
+        print(game_show)
+        for i, g in enumerate(game_show.games_played, start=1):
+            print(f'Game {i}) {g}')
 
 if __name__ == '__main__':
     main()
